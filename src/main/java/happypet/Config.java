5c524208a8f02f7e3f63e6f482d7d2c4ae314c43
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2016-10-20 10:51:25 +0100

    Fully implement config loading

--- src/main/java/happypet/Config.java
+++ src/main/java/happypet/Config.java
@@ -1,11 +1,13 @@
 package happypet;
 
+import happypet.util.ConfigUtils;
 import org.bukkit.Material;
-import org.bukkit.configuration.Configuration;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 
-import static happypet.HappyPet.*;
+import static happypet.HappyPet.LOGGER;
+import static happypet.HappyPet.PLUGIN;
 
 /** Container and manager class for plugin's configuration values */
 public class Config
@@ -18,29 +20,29 @@ public class Config
     public final Pets pets = new Pets();
     public class Pets
     {
-        public boolean ocelots = true;
-        public boolean wolves  = true;
-        public boolean horses  = true;
-        public boolean llamas  = true;
+        public boolean ocelots;
+        public boolean wolves;
+        public boolean horses;
+        public boolean llamas;
     }
 
     public final Ownership ownership = new Ownership();
     public class Ownership
     {
-        public int     maximum        = -1;
-        public boolean ignoreCreative = true;
-        public boolean nameOnClaim    = true;
-        public boolean autoUntame     = true;
-        public boolean claimOnTame    = true;
-        public boolean claimOnName    = true;
-        public boolean claimOnRide    = true;
-        public boolean claimOnSaddle  = true;
+        public int     maximum;
+        public boolean ignoreCreative;
+        public boolean nameOnClaim;
+        public boolean autoUntame;
+        public boolean claimOnTame;
+        public boolean claimOnName;
+        public boolean claimOnRide;
+        public boolean claimOnSaddle;
     }
 
     public final Tools tools = new Tools();
     public class Tools
     {
-        public Material wand = Material.BONE;
+        public Material wand;
     }
 
     public final Commands commands = new Commands();
@@ -52,46 +54,84 @@ public class Config
     public final Recipes recipes = new Recipes();
     public class Recipes
     {
-        public ShapedRecipe saddle = new ShapedRecipe(ITEM_SADDLE)
-            .setIngredient('L', Material.LEATHER)
-            .setIngredient('I', Material.IRON_INGOT)
-            .setIngredient('x', Material.AIR)
-            .shape("LLL", "LIL", "IxI");
-
-        public ShapedRecipe nameTag = new ShapedRecipe(ITEM_NAMETAG)
-            .setIngredient('S', Material.STRING)
-            .setIngredient('P', Material.PAPER)
-            .setIngredient('x', Material.AIR)
-            .shape("xxS", "xPx", "Pxx");
+        public ShapedRecipe saddle;
+        public ShapedRecipe nameTag;
     }
 
     public final Protection protection = new Protection();
     public class Protection
     {
-        public boolean playersUnclaimed     = false;
-        public boolean playersClaimed       = true;
-        public boolean mobsUnclaimed        = false;
-        public boolean mobsClaimed          = false;
-        public boolean suffocationUnclaimed = false;
-        public boolean suffocationClaimed   = true;
+        public boolean playersUnclaimed;
+        public boolean playersClaimed;
+        public boolean mobsUnclaimed;
+        public boolean mobsClaimed;
+        public boolean suffocationUnclaimed;
+        public boolean suffocationClaimed;
     }
 
     Config()
     {
+        load();
         populate();
         validate();
     }
 
-    private void populate()
+    private void load()
     {
         PLUGIN.saveDefaultConfig();
         PLUGIN.reloadConfig();
 
         config = PLUGIN.getConfig();
+        LOGGER.fine("Loaded config from disk");
+
+        // It would be better to track config version and do migrations, but after trying I found
+        // that comments are not preserved on config save. This is not what we want. Since the old
+        // config had only two options, it's easier just to halt plugin load.
+        if ( config.isSet("ignoreHorses") || config.isSet("wand") )
+            throw new RuntimeException("Old config file found; please delete it and reload the " +
+                "plugin to generate a new one");
+    }
+
+    private void populate()
+    {
+        pets.ocelots = config.getBoolean("pets.ocelots");
+        pets.wolves  = config.getBoolean("pets.wolves");
+        pets.horses  = config.getBoolean("pets.horses");
+        pets.llamas  = config.getBoolean("pets.llamas");
+
+        ownership.maximum        = config.getInt("ownership.maximum");
+        ownership.ignoreCreative = config.getBoolean("ownership.ignoreCreative");
+        ownership.nameOnClaim    = config.getBoolean("ownership.nameOnClaim");
+        ownership.autoUntame     = config.getBoolean("ownership.autoUntame");
+        ownership.claimOnTame    = config.getBoolean("ownership.claimOnTame");
+        ownership.claimOnName    = config.getBoolean("ownership.claimOnName");
+        ownership.claimOnRide    = config.getBoolean("ownership.claimOnRide");
+        ownership.claimOnSaddle  = config.getBoolean("ownership.claimOnSaddle");
+
+        tools.wand = ConfigUtils.getMaterial(config, "tools.wand");
+
+        commands.summonAcrossWorlds = config.getBoolean("commands.summonAcrossWorlds");
+
+        recipes.saddle  = ConfigUtils.getRecipe(config, ITEM_SADDLE, "recipes.saddle");
+        recipes.nameTag = ConfigUtils.getRecipe(config, ITEM_NAMETAG, "recipes.nameTag");
+
+        protection.playersUnclaimed     = config.getBoolean("protection.players.unclaimed");
+        protection.playersClaimed       = config.getBoolean("protection.players.claimed");
+        protection.mobsUnclaimed        = config.getBoolean("protection.mobs.unclaimed");
+        protection.mobsClaimed          = config.getBoolean("protection.mobs.claimed");
+        protection.suffocationUnclaimed = config.getBoolean("protection.suffocation.claimed");
+        protection.suffocationClaimed   = config.getBoolean("protection.suffocation.claimed");
+        LOGGER.fine("Populated all config classes from loaded config");
     }
 
     private void validate()
     {
-        
+        if (tools.wand == null)
+        {
+            tools.wand = Material.BONE;
+            LOGGER.warn("Using default wand material of BONE");
+        }
+
+        LOGGER.fine("Validated all loaded config");
     }
 }

95215de0f56ee1809c0b348fc9ca0998bbd78c0a
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2016-10-22 08:22:18 +0100

    Move HPLog to util/Log

--- src/main/java/happypet/util/Log.java
+++ src/main/java/happypet/util/Log.java
@@ -1,54 +1,54 @@
-package happypet;
+package happypet.util;
 
 import java.util.logging.Logger;
 
 /** Utility class for logging syntactic sugar */
-public class HPLog
+public class Log
 {
     private final Logger logger;
 
-    public HPLog(Logger logger)
+    public Log(Logger logger)
     {
         this.logger = logger;
     }
 
     /** Logs a formattable finest message using the given objects */
-    public HPLog finest(String msg, Object... args)
+    public Log finest(String msg, Object... args)
     {
         logger.finest(String.format(msg, args));
         return this;
     }
 
     /** Logs a formattable finer message using the given objects */
-    public HPLog finer(String msg, Object... args)
+    public Log finer(String msg, Object... args)
     {
         logger.finer(String.format(msg, args));
         return this;
     }
 
     /** Logs a formattable fine message using the given objects */
-    public HPLog fine(String msg, Object... args)
+    public Log fine(String msg, Object... args)
     {
         logger.fine(String.format(msg, args));
         return this;
     }
 
     /** Logs a formattable info message using the given objects */
-    public HPLog info(String msg, Object... args)
+    public Log info(String msg, Object... args)
     {
         logger.info( String.format(msg, args) );
         return this;
     }
 
     /** Logs a formattable warning message using the given objects */
-    public HPLog warn(String msg, Object... args)
+    public Log warn(String msg, Object... args)
     {
         logger.warning( String.format(msg, args) );
         return this;
     }
 
     /** Logs a formattable severe message using the given objects */
-    public HPLog err(String msg, Object... args)
+    public Log err(String msg, Object... args)
     {
         logger.severe( String.format(msg, args) );
         return this;
